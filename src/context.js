a493a567d7983822ac0b08edebae8db189af261e
rahulrwt09 <golubook09@gmail.com>
2023-04-24 05:09:24 +0100

    fetch api in context api

--- src/context.js
+++ src/context.js
@@ -1,6 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react"; //http://www.omdbapi.com/?apikey=1c0ba39a&s=titanic
+const API_URL =`http://www.omdbapi.com/?apikey=1c0ba39a&s=titanic`
 const AppContext= React.createContext();
+
 const AppProvider= ({children})=>{
+    const getMovies= async(url)=>{
+     try{
+        const res=  await fetch(url)
+        const data=res.json()
+        console.log(data)
+    }
+     catch(error){
+       console.log(error);
+     }
+
+    };
+    useEffect(()=>{
+      getMovies(API_URL);
+    },[])
     return <AppContext.Provider value="abcD">{children}</AppContext.Provider>
 
 }
