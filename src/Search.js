bfd6a7a636cc0449d130374454e7c13ad09a1c9e
rahulrwt09 <golubook09@gmail.com>
2023-04-24 12:28:58 +0100

    create search bar

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { useGlobalContext } from './context'
 
 const Search = () => {
+  const {query,setquery, iserror}= useGlobalContext();
   return (
+   <>
+    <section className='search-section'>
+    <h2>Search Movie</h2>
+    <form action='#' onSubmit={(e)=>{
+      e.preventDefault();
+    }}>
+    
     <div>
-       <p>search</p>
+      <input type="text" placeholder='search movie'
+       value={query}
+      onChange={(e)=>setquery(e.target.value)}
+    
+      />
     </div>
+    </form>
+
+    <div>
+    {console.log(iserror)}
+      <p>{iserror.show &&  iserror.msg}</p>
+    </div>
+    </section>
+   </>
   )
 }
 
--- src/context.js
+++ src/context.js
@@ -1,12 +1,13 @@
 import React, { useContext, useEffect, useState } from "react"; //http://www.omdbapi.com/?apikey=1c0ba39a&s=titanic
-const API_URL =`http://www.omdbapi.com/?apikey=1c0ba39a&s=titanic`
+const API_URL =`http://www.omdbapi.com/?apikey=1c0ba39a`
 const AppContext= React.createContext();
 
 const AppProvider= ({children})=>{
 
     const[isload,setload]=useState(true)
     const[movie,setmovie]=useState([]);
-    const[iserror,seterror]=useState({show:"false",msg:" "})
+    const[iserror,seterror]=useState({show:false,msg:" "})
+    const [query, setquery]=useState("avenger");
     const getMovies= async(url)=>{
      try{
         const res=  await fetch(url)
@@ -30,10 +31,16 @@ const AppProvider= ({children})=>{
 
     };
     useEffect(()=>{
-      getMovies(API_URL);
-    },[])
+        // getMovies(`${API_URL}&s=${query}`);
+          let timer = setTimeout(()=>{
+            getMovies(`${API_URL}&s=${query}`);
+          },1000);
+         return ()=>clearTimeout(timer)
+  
+      
+    },[query]);
    
-    return <AppContext.Provider value={{iserror,isload,movie}}>{children}</AppContext.Provider>
+    return <AppContext.Provider value={{iserror,isload,movie,query,setquery}}>{children}</AppContext.Provider>
 
 }
 const useGlobalContext=()=>{
